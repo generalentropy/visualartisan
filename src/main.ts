116b55edc4468ffd497a07e9602382f7872c73b2
entropie <entropie@tuta.io>
2024-11-18 21:16:55 +0100

    update

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "./main.css";
+import {
+  createIcons,
+  Instagram,
+  Linkedin,
+  Github,
+  Send,
+  Palette,
+  Sun,
+  Moon,
+} from "lucide";
+
+const footer = document.getElementById("footer") as HTMLElement;
+
+footer.textContent = `© ${new Date().getFullYear()} - { VisualArtisan }`;
+
+createIcons({
+  icons: { Instagram, Linkedin, Github, Send, Palette, Moon, Sun },
+
+  nameAttr: "data-lucide",
+});
